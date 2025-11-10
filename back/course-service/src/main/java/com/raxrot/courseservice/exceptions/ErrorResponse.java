19efe9f0a2f1adb77f28bd329d4abb79beabb98b
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-11-10 19:14:01 +0000

    add empty course-service

---
+++ back/course-service/src/main/java/com/raxrot/courseservice/exceptions/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.raxrot.courseservice.exceptions;
+
+import java.util.Map;
+
+public record ErrorResponse(
+        String timestamp,
+        int status,
+        String error,
+        String message,
+        String path,
+        Map<String, String> fieldErrors
+) {
+}
